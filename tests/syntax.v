cf487f699c85bea955f5e899c794bf464ef9347f
joe-conigliaro <joe@vlang.io>
2020-07-29 00:00:55 +1000

    update tests/syntax.v

--- tests/syntax.v
+++ tests/syntax.v
@@ -20,12 +20,18 @@ struct StructA {
 type SumTypeA = StructA | int | string
 
 fn fn_a() {
-    a, b := 1, 2
-	arr_a := [1,2,3,4]
-	arr_b := []string{len: 2, cap :2}
-	call_a := foo[1]()
+	a := 1
+	a, b := 1, 2
+	array_init_a := [1,2,3,4]
+	array_init_b := []string{len: 2, cap :2}
+	call_a := foo()
+	call_b := foo('string', 1, a, b)
+	call_c := foo[1]('string', 1, a, b)
 	index_a := foo.bar[1]
 	assoc_a := {struct_a|fa: fva}
+	for val_a in list_a {
+		println(val_a)
+	}
 	for key_a, val_a in list_a {
 		println(key_a)
 		println(val_a)
@@ -39,13 +45,18 @@ fn fn_a() {
 		int { println('int') }
 		string { println('string') }
 	}
+	unsafe {
+		a := 1
+		a++
+	}
 	// unsafe_a := unsafe(a++)
-	unsafe_b := unsafe {a++ a++}
+	unsafe_b := unsafe { a := 1 a++ a++ a }
 	ubsafe_c := unsafe {
 		a := 1
-		arr_a
+		a++
+		a
 	}
-	call_with_unsafe_arg_a('fo', unsafe {*deref_arg_a})
+	call_with_unsafe_as_arg_a('string', unsafe {*deref_a})
 }
 
 fn (rec &Foo) method_a() {
