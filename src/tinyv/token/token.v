42f0ae7cb6318365a822f2b8d0bc779b1bca1cf7
Joe Conigliaro <joe@vlang.io>
2021-12-24 22:16:17 +1100

    add copyright

--- src/tinyv/token/token.v
+++ src/tinyv/token/token.v
@@ -1,3 +1,6 @@
+// Copyright (c) 2020-2021 Joe Conigliaro. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 module token
 
 pub const (
--- src/tinyv/types/scope.v
+++ src/tinyv/types/scope.v
@@ -1,3 +1,6 @@
+// Copyright (c) 2020-2021 Joe Conigliaro. All rights reserved.
+// Use of this source code is governed by an MIT license
+// that can be found in the LICENSE file.
 module types
 
 // TODO: this MIGHT be moved to anohter module, here for now
